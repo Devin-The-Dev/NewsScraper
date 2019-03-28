c91b24b222cd172f78c78a2e397f9d5553abb068
Devin Udy <devin.udy@gmail.com>
2019-03-28 16:47:58 -0600

    Setup handebars and article files.

--- server.js
+++ server.js
@@ -3,23 +3,6 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const express = require("express");
 const app = express();
-const exphbs = require("express-handlebars");
-///Heroku || Localhost
-let PORT = process.env.PORT || 3000;
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "views/layouts/main.handlebars"));
-    app.listen(PORT, function () {
-        console.log("Listening on port " + PORT);
-    });
-});
-///Mongoose
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
-let db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection: error:"));
-db.once("open", function () {
-    console.log("Connected to Mongoose!");
-});
 ///
 app.use(logger("dev"));
 app.use(
@@ -27,8 +10,22 @@ app.use(
         extended: false
     })
 );
-
-
-
+//
+app.use(express.static(process.cwd() + "/public"));
+//Handlebars
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+///Mongoose
+let MONGODB_URI =/* process.env.MONGODB_URI || */"mongodb://localhost/news_scraper";
+mongoose.connect(MONGODB_URI);
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Connected to Mongoose!");
+});
+///Heroku || Localhost
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log("Listening on PORT " + PORT);
+});
